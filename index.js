16bf546894c3e00a9605724250b1c5a6ef0a1516
brazil-bruno <bruno.brazil2022@gmail.com>
2023-09-25 09:46:33 -0300

    view engine, body parser, express static

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+
+//View Engine
+app.set("view engine", "ejs");
+
+//Static files
+app.use(express.static("public"));
+
+//Body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  res.send("Welcome to my web page!.");
+  res.render("index");
 });
 
 app.listen(8080, () => {
